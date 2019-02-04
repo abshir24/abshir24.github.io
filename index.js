287578c0985d34217393592f38a3afb8395a8c00
Abshir <abshirm@live.com>
2019-02-04 08:25:23 -0800

    updated

--- index.js
+++ index.js
@@ -188,3 +188,12 @@ $(document).ready(function(){
     })
     
 })
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
