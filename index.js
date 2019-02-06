403739e9df78bc5da0ff6e9883685bd796ae78d4
Abshir <abshirm@live.com>
2019-02-06 14:57:02 -0800

    fixed scroll issues

--- index.js
+++ index.js
@@ -209,5 +209,14 @@ $(document).ready(function(){
             animate:1300
         });
     }) 
+
+    $(function() {
+        $('.js-nav a, .js-connect').click(function(e) {
+            e.preventDefault();
+            $('body, html').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+            }, 750);
+        });
+    });
 })
 
